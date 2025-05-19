7778babe807e0cc445c4d07d99e305499f05a585
jumartineze <jumartineze@unal.edu.co>
2025-05-18 20:02:27 -0500

    Test_11 solution

--- homework/pregunta_11.py
+++ homework/pregunta_11.py
@@ -6,6 +6,25 @@ utilizar pandas, numpy o scipy.
 """
 
 
+from .pregunta_01 import read_file
+from .pregunta_03 import shuffle_and_sort
+from itertools import groupby
+
+
+def mapper(lines):
+    sequence = [(col[3].split(","), int(col[1])) for col in lines]
+    sequence = [(item, sublist[1]) for sublist in sequence for item in sublist[0]]
+    return sequence
+
+
+def reducer(sequence):
+    """Reducer"""
+    result = {}
+    for key, group in groupby(sequence, lambda x: x[0]):
+        result[key] = sum(value for _, value in group)
+    return result
+
+
 def pregunta_11():
     """
     Retorne un diccionario que contengan la suma de la columna 2 para cada
@@ -13,6 +32,10 @@ def pregunta_11():
 
     Rta/
     {'a': 122, 'b': 49, 'c': 91, 'd': 73, 'e': 86, 'f': 134, 'g': 35}
-
-
     """
+    path_file = "files/input/data.csv"
+    lines = read_file(path_file)
+    sequence = mapper(lines)
+    sequence = shuffle_and_sort(sequence)
+    sequence = reducer(sequence)
+    return sequence
