66c09d584dbe0e843042e1fc55a0ff0eb632fc71
jumartineze <jumartineze@unal.edu.co>
2025-05-18 20:02:37 -0500

    Test_12 solution

--- homework/pregunta_12.py
+++ homework/pregunta_12.py
@@ -6,6 +6,18 @@ utilizar pandas, numpy o scipy.
 """
 
 
+from .pregunta_01 import read_file
+from .pregunta_03 import shuffle_and_sort
+from .pregunta_11 import reducer
+
+
+def mapper(lines):
+    sequence = [(col[0], col[4].split(",")) for col in lines]
+    sequence = [(col1, item.split(":")) for col1, col5 in sequence for item in col5]
+    sequence = [(col1, int(item[1])) for col1, item in sequence]
+    return sequence
+
+
 def pregunta_12():
     """
     Genere un diccionario que contengan como clave la columna 1 y como valor
@@ -13,5 +25,10 @@ def pregunta_12():
 
     Rta/
     {'A': 177, 'B': 187, 'C': 114, 'D': 136, 'E': 324}
-
     """
+    path_file = "files/input/data.csv"
+    lines = read_file(path_file)
+    sequence = mapper(lines)
+    sequence = shuffle_and_sort(sequence)
+    sequence = reducer(sequence)
+    return sequence
